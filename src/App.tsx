f313be3c7fd749e3d708c40b5395361644517aab
Arbnor Puka <arbnorpuka0@gmail.com>
2024-06-30 01:33:46 +0200

    Test GitHub actions

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Firebase CI/CD</h1>
+      <h1>Firebase CI/CD + GitHub Actions</h1>
     </>
   )
 }
